89df1082a3ac07afd27daba8c7ec7ce6af5e6951
Dominique Rahm <dominique.rahm@gmx.ch>
2011-03-14 20:52:35 +0100

    Ruby script for zips
    Created a rubyscript that creates a profile.zip and in this zip there is a user.js with a little
    test string

--- server/app/controllers/confChanger.rb
+++ server/app/controllers/confChanger.rb
@@ -1,14 +1,24 @@
 #!/usr/bin/ruby1.9.1
 
+require 'zip/zip'
+require 'zip/zipfilesystem'
+
 def createNewUserFile
-	filepath = Dir.pwd + "/user.js"
-	puts filepath
-	aFile = File.open(filepath, 'w')
-	aFile.puts("this is a test")
-	aFile.close	
+	zipPath = Dir.pwd + "/profile.zip"
+	filePath = Dir.pwd + "/user.js"
+
+   Zip::ZipFile.open(zipPath, Zip::ZipFile::CREATE) {
+   	|zipfile|
+   	zipfile.get_output_stream("user.js") { |f| f.puts "Hallo this is 
+	a first test creating zips with ruby!" }  
+   }
 end
 	
 	
 if __FILE__ == $0
 	createNewUserFile
 end
+
+
+
+
