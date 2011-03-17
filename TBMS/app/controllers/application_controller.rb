59834700f71d171b4064519576551905b2e409c6
Aaron Karper <akarper@students.unibe.ch>
2011-03-17 16:21:11 +0100

    Some more gems and fixing of tests

--- TBMS/app/controllers/application_controller.rb
+++ TBMS/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def index
+  end
+
+   def setHtml
+       set = params[:set]
+       @fc = FileCreator.new
+       @fc.createNewZip((set))
+   end
 end
--- TBMS/app/controllers/home_controller.rb
+++ TBMS/app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
    require 'rubygems'
    def index
+	   @users = User.all
    end
 
    def setHtml
