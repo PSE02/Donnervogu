059e95a93cd5cd927607550f551f5efd170a82d6
Jonas Ruef <jonas.ruef@gmail.com>
2011-03-22 22:42:18 +0100

    added access restrictions to index and setHtml method

--- TBMS/app/controllers/home_controller.rb
+++ TBMS/app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
    require 'rubygems'
+   
+   #grants access to the methods only for logged in user
+   before_filter :require_user, :only => [:index, :setHtml]
+
    def index
    end
 
