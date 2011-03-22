98481c13bc4850ef0a6ac7423434d7f235eef23b
Jonas Ruef <jonas.ruef@gmail.com>
2011-03-22 22:07:26 +0100

    user authentication should work use: /login /logout /signup. Next step: Customize the whole authentication process and interate in view. I use the the follwoing tutorial: http://joelgreutman.com/2011/03/13/rails-3-and-authlogic-basic-setup

--- TBMS/config/routes.rb
+++ TBMS/config/routes.rb
@@ -61,4 +61,15 @@ TBMS::Application.routes.draw do
 	root :to => "home#index"
 	match "/home/index" => "home#index"
 	match "/home/:action" => "home#setHtml"
+  
+  #routes for login/logout
+  resources :user_sessions 
+  
+  match 'login' => "user_sessions#new",      :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
+  
+  resources :users  # give us our some normal resource routes for users
+  resource :user, :as => 'account'  # a convenience route
+
+  match 'signup' => 'users#new', :as => :signup
 end
