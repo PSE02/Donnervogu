42c20f2e4c2c85c866ac8d510663962b7fa4327e
Jonas Ruef <jonas.ruef@gmail.com>
2011-03-24 21:24:52 +0100

    removed some useless files, login works now

--- TBMS/app/controllers/users_controller.rb
+++ TBMS/app/controllers/users_controller.rb
@@ -25,18 +25,4 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
-
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user # makes our views "cleaner" and more consistent
-    if @user.update_attributes(params[:user])
-      flash[:notice] = "Account updated!"
-      redirect_to account_url
-    else
-      render :action => :edit
-    end
-  end
 end
