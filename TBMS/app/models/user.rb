b7a7e2722559b17cf11b3613f4d20ab0b66d9778
Jonas Ruef <jonas.ruef@gmail.com>
2011-03-24 20:32:22 +0100

    login only with username

--- TBMS/app/models/user.rb
+++ TBMS/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
   end
   def self.find_by_username_or_email(login)
-   find_by_email(login) || find_by_login(login)
+   find_by_login(login)
 end
 end
