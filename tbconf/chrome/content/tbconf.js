2c835454f47dc23d180e3e52277db0bbbdcab203
Petar Bogdanovic <petar@smokva.net>
2011-04-02 05:23:34 +0200

    tbconf.js: add fetch(), extract(), restart()

--- tbconf/chrome/content/tbconf.js
+++ tbconf/chrome/content/tbconf.js
@@ -1,42 +1,116 @@
-/* https://developer.mozilla.org/en/Code_snippets/File_I/O */
-
+/**
+ *	https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsILocalFile
+ *	https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsIZipReader
+ *	https://developer.mozilla.org/en/XPCOM_Interface_Reference/nsIWebBrowserPersist
+ */
 function debug(msg) {
-	dump(msg+"\n");
+	dump(debug.caller.name);
+	if (msg) {
+		dump(": "+msg);
+	}
+	dump("\n");
+}
+
+function new_path() {
+	return Components.classes["@mozilla.org/file/local;1"]
+		.createInstance(Components.interfaces.nsILocalFile);
+}
+
+function new_zipr() {
+	return Components.classes["@mozilla.org/libjar/zip-reader;1"]
+		.createInstance(Components.interfaces.nsIZipReader);
+}
+
+function new_wbp() {
+	return Components.classes['@mozilla.org/embedding/browser/nsWebBrowserPersist;1']
+		.createInstance(Components.interfaces.nsIWebBrowserPersist);
+}
+
+function new_ios() {
+	return Components.classes['@mozilla.org/network/io-service;1']
+		.getService(Components.interfaces.nsIIOService);
 }
 
 function init() {
-	debug("init");
+	debug(null);
 }
 
 function fetch(uri, dest, basename) {
-	debug("fetch:"+uri+":"+basename);
+	debug(uri+", "+basename);
 
-	var path = Components.classes["@mozilla.org/file/local;1"]
-		.createInstance(Components.interfaces.nsILocalFile);
-
-	var wbp = Components.classes['@mozilla.org/embedding/browser/nsWebBrowserPersist;1']
-		.createInstance(Components.interfaces.nsIWebBrowserPersist);
-
-	var ios = Components.classes['@mozilla.org/network/io-service;1']
-		.getService(Components.interfaces.nsIIOService);
+	var path = new_path();
+	var wbp = new_wbp();
+	var ios = new_ios();
 
 	path.initWithPath(dest.path);
 	path.appendRelativePath(basename);
 	wbp.saveURI(ios.newURI(uri, null, null), null, null, null, null, path);
 }
 
-function main() {
-	debug("main");
+function extract(dest, basename) {
+	debug(basename);
 
-	var basename = "profile.zip"
-	var uri = "http://smokva.net/pse/"+basename;
-	/* var uri = "http://pse2.iam.unibe.ch/profile/john+doe@SUB.do-ma-in.co.uk"; */
+	var path = new_path();
+	var zipr = new_zipr();
+
+	path.initWithPath(dest.path);
+	path.appendRelativePath(basename);
+	zipr.open(path);
+	zipr.test(null);
+
+	var dent = zipr.findEntries("*/");
+	var fent = zipr.findEntries(null);
+
+	while (dent.hasMore()) {
+		var e = dent.getNext();
+		var f = new_path();
+		f.initWithPath(dest.path);
+		f.appendRelativePath(e);
+
+		if (d.exists()) {
+			continue;
+		}
+		debug("dir: "+f.path);
+		d.create(nsILocalFile.DIRECTORY_TYPE, 0755);
+	}
+
+	while (fent.hasMore()) {
+		var e = fent.getNext();
+		var f = new_path();
+		f.initWithPath(dest.path);
+		f.appendRelativePath(e);
+
+		debug("file: "+f.path);
+		zipr.extract(e, f);
+	}
+	zipr.close();
+}
+
+function restart() {
+	var apps = Components.interfaces.nsIAppStartup;
+	var flag = apps.eRestart | apps.eAttemptQuit;
+
+	if (!canQuitApplication()) {
+		return;
+	}
+	Components.classes["@mozilla.org/toolkit/app-startup;1"]
+		.getService(apps)
+		.quit(flag);
+}
+
+function main() {
+	debug(null);
+
 	var dest = Components.classes["@mozilla.org/file/directory_service;1"]
 		.getService(Components.interfaces.nsIProperties)
 		.get("ProfD", Components.interfaces.nsIFile);
 
+	var basename = "profile.zip"
+	var uri = "http://smokva.net/pse/"+basename;
+
 	init();
 	fetch(uri, dest, basename);
+	extract(dest, basename);
 }
 
 main();
