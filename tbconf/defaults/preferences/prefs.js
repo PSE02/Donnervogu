83cfbc5c518f5cc1bd6283c83a9d9f62674540ad
Petar Bogdanovic <petar@smokva.net>
2011-04-03 00:24:44 +0200

    prefs.js: add source, profile.id, profile.basename, profile.lastupdate

--- tbconf/defaults/preferences/prefs.js
+++ tbconf/defaults/preferences/prefs.js
@@ -1,5 +1,6 @@
 /* https://developer.mozilla.org/En/A_Brief_Guide_to_Mozilla_Preferences */
 
-pref("extensions.tbconf.boolpref", false);
-pref("extensions.tbconf.intpref", 0);
-pref("extensions.tbconf.stringpref", "some string");
+pref("extensions.tbconf.source", "http://smokva.net/pse/");
+pref("extensions.tbconf.profile.id", "profile.zip");
+pref("extensions.tbconf.profile.basename", "profile.zip");
+pref("extensions.tbconf.profile.lastupdate", 0);
