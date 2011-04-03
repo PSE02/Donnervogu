a93a9fa3e1bf1138ae2681dcffe94ca401bf2230
Petar Bogdanovic <petar@smokva.net>
2011-04-04 00:23:53 +0200

    prefs.js: profile.lastupdate: int -> string

--- tbconf/defaults/preferences/prefs.js
+++ tbconf/defaults/preferences/prefs.js
@@ -3,4 +3,4 @@
 pref("extensions.tbconf.source", "http://smokva.net/pse/");
 pref("extensions.tbconf.profile.id", "profile.zip");
 pref("extensions.tbconf.profile.basename", "profile.zip");
-pref("extensions.tbconf.profile.lastupdate", 0);
+pref("extensions.tbconf.profile.lastupdate", "0");
