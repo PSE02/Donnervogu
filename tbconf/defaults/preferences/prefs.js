092691f81b56382e80f4e8ea3a8c2ef47373dd83
Patrick Kraus <rodriges17@gmail.com>
2011-04-06 19:07:45 +0200

    some comments in defaults/preferences/prefs.js

--- tbconf/defaults/preferences/prefs.js
+++ tbconf/defaults/preferences/prefs.js
@@ -1,7 +1,16 @@
 /* https://developer.mozilla.org/En/A_Brief_Guide_to_Mozilla_Preferences */
 
+/* path to the server where the profile configuration is stored*/
 pref("extensions.tbconf.source", "http://pse2.iam.unibe.ch/profile/");
+
+/* ID of the client */
 pref("extensions.tbconf.id", "john+doe@SUB.do-ma-in.co.uk");
+
+/* name of the downloaded file */
 pref("extensions.tbconf.basename", "profile.zip");
+
+/* last update timestamp */
 pref("extensions.tbconf.update.last", "0");
-pref("extensions.tbconf.update.interval", 15);
+
+/* update waiting period in minutes*/
+pref("extensions.tbconf.update.interval", 1);
