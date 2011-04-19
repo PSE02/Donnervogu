9b2df8a82eaad31b7bb9c1f756301d1ee13c851e
Felix Langenegger <felix.langenegger@gmail.com>
2011-04-19 17:07:22 +0200

    Deleted the tbconf plugin stuff.

--- tbconf/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-/* https://developer.mozilla.org/En/A_Brief_Guide_to_Mozilla_Preferences */
-
-pref("extensions.tbconf.source", "http://pse2.iam.unibe.ch");
-pref("extensions.tbconf.profile.id", "dominique.rahm@gmx.ch");
-pref("extensions.tbconf.profile.basename", "profile.zip");
-pref("extensions.tbconf.profile.lastupdate", "0");
