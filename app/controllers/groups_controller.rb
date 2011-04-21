0ba0813e6fc939157601137600df4fa2834247c5
d3orn <dominique.rahm@gmx.ch>
2011-04-21 10:50:57 +0200

    It's not possible to delete "No Group" now (again needs some CSS for flash[:error]

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -77,8 +77,9 @@ class GroupsController < ApplicationController
   # DELETE /groups/1.xml
   def destroy
     @group = Group.find(params[:id])
-    @group.destroy
-
+    @group.destroy if @group.name != "No Group"
+    flash[:error] = "You can not delete \"No Group\"" if @group.name == "No Group"
+  
     respond_to do |format|
       format.html { redirect_to(groups_url) }
       format.xml  { head :ok }
