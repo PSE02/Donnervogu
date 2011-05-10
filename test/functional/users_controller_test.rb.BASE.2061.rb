f9d394f91cde9bfb04c02b5fe02d5b49d1c4510f
Jonas Ruef <jonas.ruef@gmail.com>
2011-05-10 19:48:05 +0200

    cleanup

---
+++ test/functional/users_controller_test.rb.BASE.2061.rb
@@ -0,0 +1,30 @@
+# Author:: Jonas Ruef
+# Needs admin account in the user fixtures, so don't delete.
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+
+	test "should create a new user" do
+    get :new
+		assert_nil assigns(:new)
+	end
+	
+	test "should prevent access if you are not a logged in user" do
+		get :show
+		assert_redirected_to '/user_sessions/new'
+	end
+
+	test "should grant you access if you are a logged in user" do
+		login_as_admin
+		get :show
+		assert_response :success
+	end
+
+	test "should create user" do
+		assert_difference "User.count" do
+			post :create, :user => { :login => "superpippo", :password => "noccioline", :password_confirmation => "noccioline"}
+			assert_redirected_to root_path
+		end
+		assert_equal "superpippo", assigns(:user).login
+	end
+end
---
+++ test/functional/users_controller_test.rb.REMOTE.2061.rb
@@ -0,0 +1,4 @@
+# Author:: Jonas Ruef
+# Needs admin account in the user fixtures, so don't delete.
+require 'test_helper'
+
